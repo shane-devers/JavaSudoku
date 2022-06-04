6d8acd2810c94e806fa9402d32ce9f287d70e814
Shane Devers <robert.a.devers@durham.ac.uk>
2022-06-04 13:05:14 +0100

    Initial commit

---
+++ src/main/java/com/shanedevers/javasudoku/Grid.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.shanedevers.javasudoku;
+import java.util.Random;
+import java.util.Arrays;
+
+/**
+ *
+ * @author shane
+ */
+public class Grid {
+    
+    private int[][] contents;
+    
+    public Grid() {
+        contents = new int[9][9];
+        Random rand = new Random();
+        for (int i = 0; i < contents.length; i++) {
+            for (int j = 0; j < contents[i].length; j++) {
+                do {
+                    contents[i][j] = rand.nextInt(9) + 1;
+                } while (checkCollision(i,j));
+            }
+        }
+    }
+    
+    public boolean checkCollision(int i, int j) {
+        if (Arrays.binarySearch(contents[i], contents[i][j]) > -1) {
+            return true;
+        }
+        for (int k = 0; k < contents.length; k++) {
+            if (contents[i][j] == contents[k][j] && i != k) {
+                return true;
+            }
+        }
+        int kLow = (i / 3) * 3;
+        int lLow = (j / 3) * 3;
+        for (int k = kLow; k < kLow + 3; k++) {
+            for (int l = lLow; l < lLow + 3; l++) {
+                if (contents[i][j] == contents[k][l] && i != k && j != l) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public int getContents(int i, int j) {
+        return contents[i][j];
+    }
+    
+}
