a3ffb9270f4d706983edb70aaeba24690418af4b
Shane Devers <robert.a.devers@durham.ac.uk>
2022-06-05 12:16:42 +0100

    Can now successfully create a Sudoku grid

--- src/main/java/com/shanedevers/javasudoku/Grid.java
+++ src/main/java/com/shanedevers/javasudoku/Grid.java
@@ -3,8 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.shanedevers.javasudoku;
-import java.util.Random;
-import java.util.Arrays;
+import java.util.*;
 
 /**
  *
@@ -19,11 +18,24 @@ public class Grid {
         contents = new int[9][9];
         size = contents.length;
         Random rand = new Random();
+        int failCount = 0;
         for (int i = 0; i < contents.length; i++) {
             for (int j = 0; j < contents[i].length; j++) {
-                do {
-                    contents[i][j] = rand.nextInt(9) + 1;
-                } while (checkCollision(i,j));
+                int choice = getValidChoice(i, j);
+                if (choice > 0) {
+                    contents[i][j] = choice;
+                } else {
+                    contents[i] = new int[9];
+                    failCount++;
+                    if (failCount < 10) {
+                        i--;
+                    } else {
+                        i = -1;
+                        contents = new int[9][9];
+                    }
+                    j = -1;
+                    break;
+                }
             }
         }
     }
@@ -49,6 +61,45 @@ public class Grid {
             }
         }
         return false;
+    }
+    
+    public int getValidChoice(int i, int j) {
+        ArrayList<Integer> valid = new ArrayList<Integer>();
+        for (int k = 1; k < 10; k++) {
+            valid.add(k);
+        }
+        for (int k = 1; k < 10; k++) {
+            for (int l = 0; l < contents[i].length; l++) {
+                if (valid.contains(k) && contents[i][l] == k && j != l) {
+                    valid.remove(Integer.valueOf(k));
+                }
+            }
+        }
+        for (int k = 1; k < 10; k++) {
+            for (int l = 0; l < contents[i].length; l++) {
+                if (valid.contains(k) && contents[l][j] == k && i != l) {
+                    valid.remove(Integer.valueOf(k));
+                }
+            }
+        }
+        int lLow = (i / 3) * 3;
+        int mLow = (j / 3) * 3;
+        for (int k = 1; k < 10; k++) {
+            for (int l = lLow; l < lLow + 3; l++) {
+                for (int m = mLow; m < mLow + 3; m++) {
+                    if (valid.contains(k) && contents[l][m] == k && (i != l || j != m)) {
+                        valid.remove(Integer.valueOf(k));
+                    }
+                }
+            }       
+        }
+        Random rand = new Random();
+        int choice = -1;
+        if (valid.size() > 0) {
+            int index = rand.nextInt(valid.size());
+            choice = valid.get(index);
+        }
+        return choice;
     }
     
     public int getContents(int i, int j) {
